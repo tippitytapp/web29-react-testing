22fbaa7e570b1b72ffc3e9b1d131fafdafc28166
Jess Martin <jessmartin@gmail.com>
2020-04-09 11:31:11 -0400

    Adding prettier and eslint and formatting some files

--- src/components/AnimalForm.js
+++ src/components/AnimalForm.js
@@ -6,52 +6,53 @@ const AnimalForm = ({ errors, touched, values }) => {
       species: 'Lion',
       age: '12',
       notes: '',
-      id: 365
-    }
+      id: 365,
+      name: '',
+    },
   ]);
   const [animal, setAnimal] = useState({ species: '', age: '', notes: '' });
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     setAnimal({ ...animal, [event.target.name]: event.target.value });
   };
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     const newAnimal = {
       ...animal,
-      id: Date.now()
+      id: Date.now(),
     };
     setAnimals([...animals, newAnimal]);
   };
 
   return (
     <div className="animal-form">
-      <form onSubmit={event => handleSubmit(event)}>
+      <form onSubmit={(event) => handleSubmit(event)}>
         <label htmlFor="species">Species:</label>
         <input
           id="species"
           type="text"
           name="species"
-          onChange={event => handleChange(event)}
+          onChange={(event) => handleChange(event)}
         />
         <label htmlFor="age">Age:</label>
         <input
           id="age"
           type="text"
           name="age"
-          onChange={event => handleChange(event)}
+          onChange={(event) => handleChange(event)}
         />
         <label htmlFor="notes">Notes:</label>
         <textarea
           id="notes"
           type="text"
           name="notes"
-          onChange={event => handleChange(event)}
+          onChange={(event) => handleChange(event)}
         />
         <button>Submit!</button>
       </form>
       <h4 data-testid="list header">Current Animals Test Species</h4>
-      {animals.map(animal => (
+      {animals.map((animal) => (
         <p data-testid={animal.species} key={animal.id}>
           {animal.species}
         </p>
